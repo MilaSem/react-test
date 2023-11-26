7b956a92583c1e4714fe6950339981396852f731
MilaSem <milafoxsem@gmail.com>
2023-11-26 20:02:45 +0300

    feat: remove react-router

--- config/tests/all_the_providers.tsx
+++ config/tests/all_the_providers.tsx
@@ -1,15 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '../../src/redux/store';
 
 import React from 'react';
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>{children}</BrowserRouter>
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
 
 export { AllTheProviders };
