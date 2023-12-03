2b13aae1e8633da4d54f51bd26540886ee97088f
MilaSem <milafoxsem@gmail.com>
2023-12-03 03:58:39 +0300

    chore: add formhook & page404 temp components

---
+++ src/components/FormHook.tsx
@@ -0,0 +1,13 @@
+const FormHook = () => {
+  return (
+    <>
+      <h1>Controlled Components Form</h1>
+      <div className="link__wrapper">
+        <a href={'/'}>to main</a>
+      </div>
+      <div>Sorry, this place are empty</div>
+    </>
+  );
+};
+
+export { FormHook };
---
+++ src/components/Page404.tsx
@@ -0,0 +1,10 @@
+function Page404() {
+  return (
+    <>
+      <div>Ooops</div>
+      <a href={`/`}>to main</a>
+    </>
+  );
+}
+
+export { Page404 };
