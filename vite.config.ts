f8744559f506492bb438483038e520d078bd37e5
MilaSem <milafoxsem@gmail.com>
2023-12-03 03:45:27 +0300

    chore: configure development tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@root': path.resolve(__dirname),
+    },
+  },
+});
